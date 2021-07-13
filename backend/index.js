30e2021321edf558f611154d0d57e2b8641b1de7
Sachin Srivastava <thiszsachin@gmail.com>
2021-07-14 00:10:44 +0530

    Initial Commit

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-  res.send("First Enpoint");
+  res.send("First API...");
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, console.log(`Server is Running on Port ${PORT}`));
